c772310025525a94821f47c9102165763a3f2ce5
yff-001 <yufeng.xu@live.com>
2023-09-03 00:29:08 -0400

    update

--- src/timers.c
+++ src/timers.c
@@ -16,23 +16,3 @@ void init_timer0() {
     OCR0A = 255;                                 // duty cycle on OC0A pin
     OCR0B = 255;                                // duty cyCle on OC0B pin
 }
-
-/* CTC */
-// void init_timer1() {
-//     TCCR1B |= (1 << WGM12);                     // phase and frequency correct, OCR1A as TOP
-//     TCCR1B |= (1 << CS10);                      // f=clk_io/1024
-//     TCCR1B |= (1 << CS12);
-//     TIMSK1 |= (1 <<OCIE1A);                     // enable OUTPUT COMPARE A MATCH INTERRUPT
-//     OCR1A = 31249;                              // 0.5 Hz
-// }
-
-/* PHASE CORRECT PWM */
-// void init_timer1() {
-//     TCCR1A |= (1 << COM1A1);                    // clear on match when up-counting, set on match when down-counting
-//     TCCR1A |= (1 << WGM10);                     // WGM1[3:0] = 0b1001
-//     TCCR1B |= (1 << WGM12);                     // phase and frequency correct, OCR1A as TOP
-//     TCCR1B |= (1 << CS10);                      // f=clk_io/64=250kHz
-//     // TCCR1B |= (1 << CS11);
-//     TIMSK1 |= (1 <<OCIE1A);                     // enable COMPARE MATCH A INTERRUPT
-//     OCR1A = 127;                                //
-// }
